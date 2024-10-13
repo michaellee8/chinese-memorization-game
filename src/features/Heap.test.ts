bb2f6a370a48866075eda240964baca971b9d93c
michaellee8 <ckmichael8@gmail.com>
2024-10-13 22:07:45 +0800

    chore: wip

--- src/features/Heap.test.ts
+++ src/features/Heap.test.ts
@@ -1,4 +1,4 @@
-import { expect, test } from "vitest"
+import { bench, expect, test } from "vitest"
 import { heap } from "./Heap"
 
 // ported from https://cs.opensource.google/go/go/+/refs/tags/go1.23.2:src/container/heap/heap_test.go
@@ -96,5 +96,80 @@ test("heap test", () => {
     }
     h.verify(0)
     expect(x).toBe(i)
+  }
+})
+
+test("heap test remove 0", () => {
+  let h = new MyHeap()
+  for (let i = 0; i < 10; i++) {
+    h.push(i)
+  }
+  h.verify(0)
+
+  while (h.len() > 0) {
+    let i = h.len() - 1
+    let x = heap.remove(h, i)
+    expect(x).toBe(i)
+    h.verify(0)
+  }
+})
+
+test("heap test remove 1", () => {
+  let h = new MyHeap()
+  for (let i = 0; i < 10; i++) {
+    h.push(i)
+  }
+  h.verify(0)
+
+  for (let i = 0; h.len() > 0; i++) {
+    let x = heap.remove(h, 0)
+    expect(x).toBe(i)
+    h.verify(0)
+  }
+})
+
+test("heap test remove 2", () => {
+  let N = 10
+
+  let h = new MyHeap()
+  for (let i = 0; i < N; i++) {
+    h.push(i)
+  }
+  h.verify(0)
+
+  let m = new Map<number, boolean>()
+  while (h.len() > 0) {
+    m.set(heap.remove(h, Math.floor((h.len() - 1) / 2)), true)
+    h.verify(0)
+  }
+  expect(m.size).toEqual(N)
+  for (let i = 0; i < m.size; i++) {
+    expect(m.get(i)).toBe(true)
+  }
+})
+
+test("heap test fix", () => {
+  let h = new MyHeap()
+  h.verify(0)
+
+  for (let i = 200; i > 0; i -= 10) {
+    heap.push(h, i)
+  }
+  h.verify(0)
+
+  expect(h.arr[0]).toEqual(10)
+  h.arr[0] = 210
+  heap.fix(h, 0)
+  h.verify(0)
+
+  for (let i = 100; i > 0; i--) {
+    let elem = Math.floor(Math.random() * h.len())
+    if ((i & 1) === 0) {
+      h.arr[elem] *= 2
+    } else {
+      h.arr[elem] /= 2
+    }
+    heap.fix(h, elem)
+    h.verify(0)
   }
 })
